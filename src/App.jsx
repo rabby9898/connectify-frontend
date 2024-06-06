7815f97a9f5ae4d2127d461225e6120c77b2cd21
rabby9898 <mdfajlerabby00@gmail.com>
2024-06-06 16:09:56 +0600

    post, skeletons, sidebar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/Home/HomePage";
 import SignUp from "./Pages/Auth/SignUp";
 import Login from "./Pages/Auth/Login";
+import Sidebar from "./component/common/Sidebar";
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUp />} />
