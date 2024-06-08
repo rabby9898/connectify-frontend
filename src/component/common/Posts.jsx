b1744b246a69c6d5c1b3da8099396ca222babd0a
rabby9898 <mdfajlerabby00@gmail.com>
2024-06-08 18:00:22 +0600

    update profile functionality added

--- src/component/common/Posts.jsx
+++ src/component/common/Posts.jsx
@@ -43,7 +43,7 @@ const Posts = ({ feedType, username, userId }) => {
   });
   useEffect(() => {
     refetch();
-  }, [feedType, refetch]);
+  }, [feedType, refetch, username]);
 
   return (
     <>
